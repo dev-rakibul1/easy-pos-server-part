d8a07d9a76196741485ba5fee5e15d854bc07ee2
Rakibul Islam <fl.rakibul@gmail.com>
2024-09-17 19:06:31 +0600

    Vercel dir change

--- dist/app/interfaces/pagination.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.paginationQueryKeys = void 0;
-exports.paginationQueryKeys = ['limit', 'page', 'sortby', 'sortOrder'];
